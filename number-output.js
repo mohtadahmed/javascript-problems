46cac506929d527eb0a6057546ebc6e96e281ba3
mohtadahmed <mohtadsaeem13@gmail.com>
2022-09-13 19:03:43 +0600

    showing a range of number as output

---
+++ number-output.js
@@ -0,0 +1,28 @@
+// Problem Statement
+
+// Write a program that will show all the numbers between 58 to 98.
+
+
+/*
+-----------------------
+=======================
+        Solution
+=======================
+-----------------------
+*/
+
+// Using While Loop
+
+var number = 58;
+
+while (number <= 98) {
+    console.log(number);
+    number++;
+}
+
+
+// Using For Loop
+
+for (var i = 58; i <= 98; i++){
+    console.log(i);    
+}
