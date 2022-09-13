33e695eae9d71090a899c5ffcc826b23d19ce925
mohtadahmed <mohtadsaeem13@gmail.com>
2022-09-13 19:29:41 +0600

    extracting elements from an array using for loop

---
+++ array-for-loop.js
@@ -0,0 +1,20 @@
+// Problem Statement
+
+// Make an array with all the main basic elements of web development and show all the elements of the array as an output using for loop.
+// Array items: HTML, CSS, HTML5, CSS3, Bootstrap, Tailwind, JavaScript
+
+/*
+-----------------------
+=======================
+        Solution
+=======================
+-----------------------
+*/
+
+// Declaring an Array using the given value
+var elements = ["HTML", "HTML5", "CSS", "CSS3", "Bootstrap", "Tailwind", "JavaScript"];
+
+
+for (var i = 0; i < elements.length; i++){
+    console.log(elements[i]);
+}
