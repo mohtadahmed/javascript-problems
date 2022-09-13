6e0c90ec67539e8523dac7f68c97393ac3d81a5a
mohtadahmed <mohtadsaeem13@gmail.com>
2022-09-13 22:35:43 +0600

    comparing value from the array

---
+++ continue-loop.js
@@ -0,0 +1,25 @@
+// Problem Statement
+
+// Create an Array using all your books price. Now write a program to show output of the book prices which are below 200tk. Those who have the price higher than 200tk will skip and show the rest of the prices in the output.
+
+// Book Prices: 114, 70, 240, 410, 120, 140, 155, 350, 175, 198, 270.
+
+
+/*
+-----------------------
+=======================
+        Solution
+=======================
+-----------------------
+*/
+
+var bookPrices = [114, 70, 240, 410, 120, 140, 155, 350, 175, 198, 270];
+var price = 0;
+
+
+while(price < bookPrices.length){
+    if((bookPrices[price]) < 200){
+        console.log(bookPrices[price]);
+    }
+    price++;
+}
