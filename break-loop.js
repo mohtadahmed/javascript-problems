064237981acd26cb8dd6c87872263eefb0800926
mohtadahmed <mohtadsaeem13@gmail.com>
2022-09-13 19:43:43 +0600

    break the loop

---
+++ break-loop.js
@@ -0,0 +1,20 @@
+// Problem Statement
+
+// Write a program to run a for loop between the number 30 to 86. The program need to be stop when it reached to the number 44.
+
+
+/*
+-----------------------
+=======================
+        Solution
+=======================
+-----------------------
+*/
+
+
+for(i = 30; i <= 86; i++) {
+    console.log(i);
+    if (i === 44){
+        break;
+    }
+}
