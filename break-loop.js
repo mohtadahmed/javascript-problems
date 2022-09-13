0a6f126b5f50b011d721eb930574ee7df12f69bf
mohtadahmed <mohtadsaeem13@gmail.com>
2022-09-13 19:47:52 +0600

    break the loop using For and While Loop

--- break-loop.js
+++ break-loop.js
@@ -1,6 +1,6 @@
 // Problem Statement
 
-// Write a program to run a for loop between the number 30 to 86. The program need to be stop when it reached to the number 44.
+// Write a program to run a For loop between the number 30 to 86. The program need to be stop when it reached to the number 44.
 
 
 /*
@@ -12,9 +12,24 @@
 */
 
 
+// Break the Loop using For Loop
+
 for(i = 30; i <= 86; i++) {
     console.log(i);
     if (i === 44){
         break;
     }
+}
+
+
+// Break the Loop using While Loop
+
+var startingNumber = 30;
+
+while(startingNumber <= 86){
+    console.log(startingNumber);
+    if(startingNumber === 44){
+        break;
+    }
+    startingNumber++;
 }
