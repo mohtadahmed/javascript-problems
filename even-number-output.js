ca3e0fc0a5fcff853ff3a04a2dded7d3b491f3ee
mohtadahmed <mohtadsaeem13@gmail.com>
2022-09-13 19:10:18 +0600

    even number output

---
+++ even-number-output.js
@@ -0,0 +1,27 @@
+// Problem Statement
+
+// Write a program to show all the even number between 412 to 456.
+
+/*
+-----------------------
+=======================
+        Solution
+=======================
+-----------------------
+*/
+
+// Using While Loop
+
+var number = 412;
+
+while (number <= 456) {
+    console.log(number);
+    number += 2;
+}
+
+
+//  Using For Loop
+
+for (let i = 412; i <= 456; i += 2) {
+    console.log(i);
+ }
